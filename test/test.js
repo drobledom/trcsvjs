78fd0040a614797dfd2f621705ff44d6010cdb74
rodrimtz <rodrigo@traventia.com>
2021-11-12 12:42:10 +0100

    Bug with booleans while creating csv

--- test/test.js
+++ test/test.js
@@ -1,39 +1,31 @@
-
 var trcsvjs = require("../src/trcsvjs");
 
+var simpleCsv = require("fs").readFileSync("./test/csv_1.csv", "utf-8");
 
-var simpleCsv=require('fs').readFileSync('prueba2.csv', 'utf-8');
-//console.log(simpleCsv);
-trcsvjs.csvStringToJs(simpleCsv,function(err,trcsv){
-	//console.log(trcsv.getRow(2));
+trcsvjs.csvStringToJs(simpleCsv, function (err, trcsv) {
+  //console.log(trcsv.getRow(2));
 
-	var spec = {a:'',b:'dz',c:'fz',d:'dt',e:'b',f:'sc',g:'ccy'};
-	var spec = {
-"provider" :'',
-"code" :'',
-"name" :'',
-"address" :'',
-"province" :'',
-"city" :'',
-"latitude" :'fz',
-"longitude" :'fz',
-"zone_code" :'',
-"country" :'',
-"cat" :'',
-"cp" :'',
-"phone" :'',
-"lock" :'bz',
-"blacklist" :'bz',
-};
-	/*for(var i=1;i<=trcsv.number_of_rows;i++){
-		console.log(trcsv.getRow(i));
-		var obj = trcsv.getObjFromRow(i,spec);
-		console.log(obj);
-		console.log(trcsv.getRowFromObj(obj,spec));
+  var spec = { a: "", b: "dz", c: "fz", d: "dt", e: "b", f: "sc", g: "ccy" };
 
-	}*/
+  //Upload
+  const csvAsJson = [];
+  for (var i = 1; i <= trcsv.number_of_rows; i++) {
+    //console.log(trcsv.getRow(i));
+    var obj = trcsv.getObjFromRow(i, spec);
+    //console.log(trcsv.getRowFromObj(obj, spec));
 
-	/*
+    csvAsJson.push(obj);
+  }
+  //console.log(csvAsJson);
+
+  //Download
+  const csv = trcsvjs.getEmptyCsv({ delimiter: ";" });
+  csvAsJson.forEach((thisdata) => {
+    csv.insertRowFromObj(thisdata, spec);
+  });
+  //console.log(csv);
+
+  /*
 
 	var csv = trcsvjs.getEmptyCsv('|','');
 	csv.insertRow([1,2]);
@@ -48,5 +40,4 @@ trcsvjs.csvStringToJs(simpleCsv,function(err,trcsv){
 	console.log(csv.getCell(5,2));
 
 	console.log(csv.getCsvText());*/
-
 });
